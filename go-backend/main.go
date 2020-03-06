2c0709eb3cabcaf92fc206a64c3e2d62dfb8d2e8
zal1358@gmail.com <zal1358@gmail.com>
2020-03-06 12:16:14 +0200

    go beginning

--- go-backend/main.go
+++ go-backend/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	fmt.Printf("Local machine time is: %s \n", time.Now())
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
 }
